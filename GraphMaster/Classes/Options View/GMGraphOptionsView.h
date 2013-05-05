3f70dbb2c9277dcdc1bc50c25400021db56b1796
John Newman <john@Johns-MacBook-Air.local>
2013-05-05 16:29:18 -0400

    Adding Graph options view.

---
+++ GraphMaster/Classes/Options View/GMGraphOptionsView.h
@@ -0,0 +1,13 @@
+//
+//  GMGraphOptionsView.h
+//  GraphMaster
+//
+//  Created by John Newman on 4/30/13.
+//  Copyright (c) 2013 John Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMGraphOptionsView : UIView
+
+@end
---
+++ GraphMaster/Classes/Options View/GMGraphOptionsView.m
@@ -0,0 +1,31 @@
+//
+//  GMGraphOptionsView.m
+//  GraphMaster
+//
+//  Created by John Newman on 4/30/13.
+//  Copyright (c) 2013 John Newman. All rights reserved.
+//
+
+#import "GMGraphOptionsView.h"
+
+@implementation GMGraphOptionsView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
